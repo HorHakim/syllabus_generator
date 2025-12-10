63a240b83a1253327e9c89bae1f072b7081e60c3
Hakim <horairyhakim@gmail.com>
2025-12-10 13:15:51 +0100

    feature: frontend implementation

---
+++ frontend.py
@@ -0,0 +1,41 @@
+import streamlit
+from syllabus_agent import SyllabusAgent 
+
+
+
+if "syllabus_agent" not in streamlit.session_state :
+	streamlit.session_state.syllabus_agent = SyllabusAgent()
+
+
+def init_header():
+	streamlit.set_page_config(page_title="Syllabus Terminator", page_icon="🤖")
+	streamlit.title("💖 Ton générateur de syllabus préféré !")
+
+
+
+
+def show_discussion_history(history_placeholder):
+	container = history_placeholder.container()
+	with container:
+		for message in streamlit.session_state.syllabus_agent.history:
+			if message["role"] != "system":
+				with streamlit.chat_message(message["role"]):
+					streamlit.write(message["content"])
+
+
+def user_interface():
+	init_header()
+	history_placeholder = streamlit.empty() 
+	show_discussion_history(history_placeholder)
+	with streamlit.container():
+		
+		user_input = streamlit.chat_input("Copie colle ton pavet je m'occupe de tout bg !")
+
+		if user_input:
+			streamlit.session_state.syllabus_agent.ask_llm(user_interaction=user_input, model=selected_model)
+			show_discussion_history(history_placeholder)
+
+
+
+if __name__ == "__main__":
+	user_interface()
---
+++ syllabus_agent.py
@@ -0,0 +1,3 @@
+class SyllabusAgent:
+	def __init__(self):
+		self.history = {}
